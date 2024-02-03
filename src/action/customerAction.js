6800198a6fd187f847523f91c43bfcfaa6d81a88
danilo012 <46241643+danilo012@users.noreply.github.com>
2024-02-04 01:25:39 +0800

    Update customerAction.js

--- src/action/customerAction.js
+++ src/action/customerAction.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url='https://dct-billing-app.herokuapp.com/api/customers'
+const url='https://dct-billing-app.onrender.com/api/customers'
 
 export const setCustomers = (data) => {
     return {
